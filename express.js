df9d96996ada632f2fbf50138da04db48d1e04d3
JawadMM <almuttawa.j@hotmail.com>
2023-12-03 17:55:10 +0300

    be_3-exercise_4

--- express.js
+++ express.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
@@ -14,21 +14,45 @@ const port = 3000;
 //   console.log(`Server is running on port ${port}`);
 // });
 
-
 const rootRouter = express.Router();
 
-rootRouter.get('/', (req, res) => {
-  res.send('Hello, world!');
+rootRouter.get("/", (req, res) => {
+  res.send("Hello, world!");
 });
 
 const usersRouter = express.Router();
 
-usersRouter.post('/', (req, res) => {
-  res.send('User created successfully!');
+usersRouter.post("/", (req, res) => {
+  res.send("User created successfully!");
 });
 
-app.use('/', rootRouter);
-app.use('/users', usersRouter);
+app.use("/", rootRouter);
+app.use("/users", usersRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.use(express.urlencoded({ extended: false }));
+
+rootRouter.get("/", (req, res) => {
+  res.send(`
+    <form method="POST" action="/submit">
+      <input type="text" name="username" placeholder="Username" />
+      <button type="submit">Submit</button>
+    </form>
+  `);
+});
+
+const submitRouter = express.Router();
+
+submitRouter.post("/", (req, res) => {
+  const username = req.body.username;
+  res.send(`Form submitted successfully! Username: ${username}`);
+});
+
+app.use("/", rootRouter);
+app.use("/submit", submitRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
