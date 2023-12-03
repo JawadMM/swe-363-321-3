ddfb44120851196439e3fe784aafa046aef08b07
JawadMM <almuttawa.j@hotmail.com>
2023-12-03 17:51:27 +0300

    be_3-exercise_3

--- express.js
+++ express.js
@@ -1,14 +1,37 @@
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello, world!');
+// });
+
+// app.post('/users', (req, res) => {
+//   res.send('User created successfully!');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+const rootRouter = express.Router();
+
+rootRouter.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.post('/users', (req, res) => {
+const usersRouter = express.Router();
+
+usersRouter.post('/', (req, res) => {
   res.send('User created successfully!');
 });
+
+app.use('/', rootRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
